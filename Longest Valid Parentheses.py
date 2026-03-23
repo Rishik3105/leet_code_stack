09540f57faa9482cc7c17f15756c76d7e34ddb8c
Nimmani Rishik <nimmanirishik@gmail.com>
2026-03-23 19:12:23 +0530

    Longest Valid Parentheses.py

---
+++ Longest Valid Parentheses.py
@@ -0,0 +1,21 @@
+def longestValidParentheses(s):
+    stack = [-1]
+    max_len = 0
+
+    for i in range(len(s)):
+        if s[i] == '(':
+            stack.append(i)
+        else:
+            stack.pop()
+
+            if not stack:
+                stack.append(i)
+            else:
+                max_len = max(max_len, i - stack[-1])
+
+    return max_len
+
+if __name__ == "__main__":
+    s = input("Enter parentheses string: ")
+    result = longestValidParentheses(s)
+    print("Longest valid parentheses length:", result)
